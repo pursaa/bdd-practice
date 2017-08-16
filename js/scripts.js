c2175c4c3cfc55f4990a973ecdff013e3a8cea92
Debbie Michel and Safiy Cham <debbie@>
2017-08-16 10:43:49 -0700

    Get results to show false on html

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,14 @@
+var leapYear = function(year) {
+  return false;
+};
+
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
-
-    //business logic
-// var leapYear = function(year) {
-//   if (year % 4===0) {
-//   return true;
-// }else {
-//   return false;
-// }
-// console.log ("leapYear");
-//
-// };
-//user interface logic
-var year =parseInt($('input#year').val());
-var result = leapYear(year);
-$("#result").text(result);
-
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+    // console.log (result);
 
   });
 });
