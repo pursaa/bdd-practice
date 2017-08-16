1da6086d9121f8c753a895e11a3848eaff184071
Debbie Michel and Safiy Cham <debbie@>
2017-08-16 11:25:06 -0700

    Refactor results

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,29 @@
 //   return false;
 // };
 
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  }else {
+    return false;
+  }
+};
+
 // var leapYear = function(year) {
-//   if (year % 4 === 0){
+//   if (year % 100 === 0){
+//     return false;
+//   }else {
+//     return true;
+//   }
+// };
+
+// var leapYear = function(year) {
+//   if (year % 400 === 0){
 //     return true;
 //   }else {
 //     return false;
 //   }
 // };
-
-var leapYear = function(year) {
-  if (year % 100 === 0){
-    return false;
-  }else {
-    return true;
-  }
-};
 
 
 
