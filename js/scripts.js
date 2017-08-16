d13d1cdd332da8fa19601b236deb8a670a522928
Debbie Michel and Safiy Cham <safiy@>
2017-08-16 11:06:27 -0700

    Get code to return false when year is divisable by 100

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,19 @@
 //   return false;
 // };
 
+// var leapYear = function(year) {
+//   if (year % 4 === 0){
+//     return true;
+//   }else {
+//     return false;
+//   }
+// };
+
 var leapYear = function(year) {
-  if (year % 4 === 0){
-    return true;
-  }else {
+  if (year % 100 === 0){
     return false;
+  }else {
+    return true;
   }
 };
 
