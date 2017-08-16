6c9df1e0292b31da3fa6d127d977abfb9b3ad494
Debbie Michel and Safiy Cham <debbie@>
2017-08-16 11:26:19 -0700

    Remove extra spaces

--- js/scripts.js
+++ js/scripts.js
@@ -26,16 +26,12 @@ var leapYear = function(year) {
 //   }
 // };
 
-
-
-
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
     $("#result").text(result);
-    // console.log (result);
 
   });
 });
